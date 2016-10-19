c0e3fff6f4f3b559bb8152c3676e43b8a0237344
Ralph L <losuffi@hotmail.com>
2016-10-19 17:30:24 +0800

    人物背包的UI构图（关联人物背包的逻辑代码，实时更新打开背包后的显示类容，包括背包格子，物品图标以及物品数目）

---
+++ Pocket.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pocket : MonoBehaviour {
+
+    // Use this for initialization
+    public Sprite[] Item_image;
+    private int[] U_Item_id=new int[20];
+    private int[] U_Item_count=new int[20];
+    private bool isUpdate;
+    private bool isOpen;
+    private float delay;
+    private float pretime;
+	void Start () {
+        delay = 0.5f;
+        for (int i = 1; i < 10; i++)
+        {
+            this.transform.parent.Find("Pocket").FindChild("tP_" + i.ToString()).gameObject.SetActive(false);
+        }
+        this.transform.parent.Find("Pocket").gameObject.SetActive(false);
+        isOpen = false;
+        pretime = 0;
+        isUpdate = false;
+	}
+    private void Update_canvas()
+    {
+        if (isUpdate)                               
+        {
+            for (int j = 0; j < U_Item_count.Length; j++)
+            {
+                if (U_Item_count[j] > 0)
+                {
+                    this.transform.parent.Find("Pocket").FindChild("tP_" + (j+1).ToString()).gameObject.SetActive(true);
+                    this.transform.parent.Find("Pocket").FindChild("tP_" + (j+1).ToString()).GetComponent<Text>().text = U_Item_count[j].ToString();
+                }
+                if (U_Item_id[j] - 20001 > -1)
+                {
+                    this.transform.parent.Find("Pocket").FindChild("P_" + (j + 1).ToString()).GetComponent<Image>().sprite = Item_image[U_Item_id[j] - 20001];
+                }
+            }
+            isUpdate = false;
+        }
+    }
+    public void Update_canvas(int[] id,int[] adr)
+    {
+        isUpdate = true;        
+        U_Item_id = (int[])id.Clone();
+        U_Item_count = (int[])adr.Clone();
+        if (isOpen)                                                 //更新背包，背包打开时获得物品
+        {
+            this.transform.parent.Find("Pocket").gameObject.SetActive(true);
+            this.Update_canvas();
+        }
+    }
+    void Poc_control()
+    {
+        if (this.gameObject.GetComponent<OnButtonPressed>().Deal == "KeyDown" && Time.time - pretime > delay)
+        {
+            pretime = Time.time;
+            isOpen = (isOpen == false ? true : false);
+            if (isOpen)
+            {                                                                   //更新背包，背包关闭时获得物品
+                this.transform.parent.Find("Pocket").gameObject.SetActive(true);
+                this.Update_canvas();
+            }
+            else
+                this.transform.parent.Find("Pocket").gameObject.SetActive(false);
+        }
+
+    }
+    void Update()
+    {
+        Poc_control();
+    }
+}
