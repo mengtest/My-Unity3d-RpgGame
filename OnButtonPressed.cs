f08b0e9d863bbe346d0515eabd6e816ee89a8bdb
Ralph L <losuffi@hotmail.com>
2016-10-19 17:16:34 +0800

    android 虚拟键模拟(方向键+A/B键+跳跃键+背包UI打开)

---
+++ OnButtonPressed.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+public class OnButtonPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{  
+    private float delay = 0.05f;
+    private bool isDown;
+    private float downTIme;
+    public string Deal;
+    public string Status;
+    void Start()
+    {
+        Deal = "KeyFree";
+        Status = "KeyFree";
+        downTIme = 0;
+        isDown = false;
+    }
+    void Update()
+    {
+        if (isDown && Time.time - downTIme > delay)
+        {
+            Status = "KeyUp";
+            //downTIme = Time.time;
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Deal = "KeyDown";
+        Status = "KeyDown";
+        downTIme = Time.time;
+        isDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Deal = "KeyUp";
+        Status = "KeyFree";
+        isDown = false;
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Deal = "KeyFree";
+        Status = "KeyFree";
+        isDown = false;
+    }
+}
