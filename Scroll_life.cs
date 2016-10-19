40574595bde3521a9e8fca4eaa104d22a576d3b2
Ralph L <losuffi@hotmail.com>
2016-10-19 17:22:46 +0800

    人物的血条（逻辑关联，以及实时血条UI变化）

---
+++ Scroll_life.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Scroll_life : MonoBehaviour {
+    private float plife;
+    private float slife;
+	// Use this for initialization
+	void Start () {
+        plife = GameObject.Find("player").gameObject.GetComponent<player_life>().p_life;
+        slife= GameObject.Find("player").gameObject.GetComponent<Self_class>().s_life;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        plife = GameObject.Find("player").gameObject.GetComponent<player_life>().p_life;
+        slife = GameObject.Find("player").gameObject.GetComponent<Self_class>().s_life;
+        this.gameObject.GetComponent<Scrollbar>().size = plife / slife;
+        this.transform.Find("T_pl").GetComponent<Text>().text = ((int)plife).ToString() + "/" + ((int)slife).ToString();
+	}
+}
---
+++ player_life.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_life : MonoBehaviour {
+
+    public float p_life;
+    public Animator ani;
+	void Start () {
+        p_life = this.gameObject.GetComponent<Self_class>().s_life;
+	}
+	
+	void Update () {
+        //if(p_life>0)
+        //    p_life -= 2f;
+        if (p_life < 1)
+           this.gameObject.GetComponent<player>().isLife = false;
+
+    }
+}
