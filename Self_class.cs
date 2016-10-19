8dcac18342e688d40f645eaca5831e65754b81c5
Ralph L <losuffi@hotmail.com>
2016-10-19 17:25:05 +0800

    新增自定义游戏对象类（包括定义 游戏物体的种类（如物品、英雄）；血量，攻击，Id等）

---
+++ Self_class.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Self_class : MonoBehaviour {
+    public string s_class;
+    public int s_id;
+    public float s_life;
+    public float s_speed;
+    void Start()
+    {
+
+    }
+}
+/*s_class:
+ *        Master:怪物类
+ *        Item:物品类
+ *        
+ *        
+ * 
+ * s_id:
+ *         10->:怪物类
+ *         20->:物品类
+ *       
+ *       20：
+ *          20001：木材
+ *          20002：石材
+ *          20003：动物皮毛
+ *          20004：草藤
+ * */
