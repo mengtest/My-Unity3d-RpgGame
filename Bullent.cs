ee7ca3a922864377d26d741de6627dcda70d8282
Ralph L <losuffi@hotmail.com>
2016-10-22 23:57:54 +0800

    子弹飞行控制，碰撞检测，子弹生存周期

---
+++ Bullent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullent : MonoBehaviour {
+    private float starttime;
+    void Start()
+    {
+        starttime = Time.time;
+        this.transform.position += Vector3.up * 2;
+    }
+
+    void Update()
+    {
+        //this.transform.position += transform.forward * Time.deltaTime;
+        //if (Time.time - starttime > 10)
+        //{
+        //    Destroy(this.gameObject);
+        //}
+    }
+}
