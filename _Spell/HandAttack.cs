4656cfbbd73567057b8e4b707167a340bfde3a3e
Ralph L <losuffi@hotmail.com>
2016-10-22 23:53:33 +0800

    技能树模型，实现各个技能的注册，为单位调用技能提供接口，目前设有两大技能种类（自身技能，目标技能）

---
+++ _Spell/HandAttack.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandAttack : MonoBehaviour
+{
+    public Transform Bullent;
+    public void Work(Transform Trig_Unit)
+    {
+        int m = 0;
+        Transform[] Target = new Transform[10];
+        Trig_Unit.GetComponent<Animator>().SetBool("isAttack",true);
+        StartCoroutine(this.Work_on(Target, m,Trig_Unit));
+    }
+    private IEnumerator Work_on(Transform []Target,int m,Transform Trig_Unit)
+    {
+        yield return new WaitForSeconds(0.7f);
+        Vector3 Trig_point = Trig_Unit.position;
+        RaycastHit hit;
+        bool isCover = false;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                Trig_point = Trig_Unit.position + k * Trig_Unit.right + j * Trig_Unit.up - Trig_Unit.right - 0.2f * Trig_Unit.up;
+                //Instantiate(Bullent, Trig_point, Trig_Unit.rotation);
+                if (Physics.Linecast(Trig_point, Trig_point + Trig_Unit.forward * 2, out hit))
+                {
+                    isCover = false;
+                    foreach (Transform ta in Target)
+                    {
+                        if (ta == hit.collider.transform || hit.collider.transform == Trig_Unit)
+                        {
+                            isCover = true;
+                            break;
+                        }
+                    }
+                    if (!isCover)
+                    {
+                        Target[m] = hit.collider.transform;
+                        m++;
+                    }
+
+                }
+            }
+        }
+        for (int n = 0; n < m; n++)
+        {
+            if (Target[n].GetComponent<Self_class>().s_class == "Hero"&& Target[n].GetComponent<Self_class>().isLife==true)
+            {
+                Debug.Log(Target[n].name + " attacked");
+                Target[n].GetComponent<Self_class>().injured(Trig_Unit.GetComponent<Self_class>().s_AttackValue);
+            }
+        }
+    }
+}
